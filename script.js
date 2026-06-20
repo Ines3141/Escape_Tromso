7c06923a17ff58892c074f49f532ea5b4ef986fc
JKnuds1 <Jknuds1@hotmail.com>
2026-06-20 14:51:54 +0200

    Changed the folder structure for the html files. Tried to update everything so all the links works but may have missed some. To fix it just update wathever link is not working

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkCode() {
   let code = document.getElementById("codeInput").value;
 
   if (code === "TROMSO") {
-    window.location.href = "Stations/HTML/station_1.html";
+    window.location.href = "stations/station_1.html";
   } else {
     document.getElementById("message").textContent = "Wrong code. Try again.";
   }

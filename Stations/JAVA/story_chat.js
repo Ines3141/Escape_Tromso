e4429cdcabe1372f26bf03b0b7aa9b0ff4e533ed
InesS <inesseeliger7@gmail.com>
2026-08-31 15:53:09 +0200

    Padding

--- Stations/JAVA/story_chat.js
+++ Stations/JAVA/story_chat.js
@@ -138,7 +138,7 @@
                above the bottom navigation on phones */
             @media (max-width: 600px) {
                 #messages {
-                    padding-bottom: 120px;
+                    padding-bottom: 220px;
                 }
             }
 
